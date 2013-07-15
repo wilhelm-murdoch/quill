417baedcddf9aaefb0c444770a5bbf24aec8f82a
Wilhelm Murdoch <wilhelm.murdoch@gmail.com>
2013-07-16 01:07:20 +1000

    Playing around with angular.

--- quill/static/javascript/angular/app.js
+++ quill/static/javascript/angular/app.js
@@ -1,1 +1,39 @@
-quill = angular.module('quill', [])
+angular.module('Quill', [])
+
+  .controller('QuillCtrl', ['$rootScope', 'Inkwell', '$http', function($rootScope, Inkwell, $http) {
+    $http.defaults.headers.common.Accept = 'application/json'
+  }])
+
+  .controller('QuillIndexCtrl', ['$scope', 'Inkwell', function($scope, Inkwell) {
+    $scope.articles = Inkwell.get().success(function(resource) {
+      $scope.articles = resource
+    })
+  }])
+
+  .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+        controller: 'QuillIndexCtrl'
+      , templateUrl: 'static/javascript/angular/views/home.html'
+    }).otherwise({
+      redirectTo: '/'
+    })
+  }])
+
+  .factory('Inkwell', ['$http', function($http) {
+    return {
+      get: function(year, month, day, title) {
+        var url = '/inkwell'
+        if(year) {
+          url += '/' + year
+        } else if(year && month) {
+          url += '/' + year + '/' + month
+        } else if(year && month && day) {
+          url += '/' + year + '/' + month + '/' + day
+        } else if(year && month && day && title) {
+          url += '/' + year + '/' + month + '/' + day + '/' + title
+        }
+
+        return $http.get(url)
+      }
+    }
+  }])
