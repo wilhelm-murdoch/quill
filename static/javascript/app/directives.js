f9e57e4edcf48843711a3f1372b9b7ee97348957
Wilhelm Murdoch <wilhelm.murdoch@gmail.com>
2013-08-04 02:55:07 +1000

    Again, more code cleanup. Header and footer are now directives. Yay.

--- static/javascript/app/directives.js
+++ static/javascript/app/directives.js
@@ -5,6 +5,22 @@ Quill.directive('time', function() {
   }
 })
 
+Quill.directive('siteHeader', function() {
+  return {
+      restrict: 'E'
+    , replace: true
+    , templateUrl: 'static/javascript/app/templates/partials/header.html'
+  }
+})
+
+Quill.directive('siteFooter', function() {
+  return {
+      restrict: 'E'
+    , replace: true
+    , templateUrl: 'static/javascript/app/templates/partials/footer.html'
+  }
+})
+
 Quill.directive('article', ['$timeout', function($timeout) {
   return {
       restrict: 'E'
